cd3c907bd206a6edb9736d201c82130305db3a33
Arash <161996381+ArashAfkandeh@users.noreply.github.com>
2025-07-30 18:47:35 +0330

    Update install_ocserv.sh

--- install_ocserv.sh
+++ install_ocserv.sh
@@ -6,7 +6,7 @@
 # Usage (non-interactive): sudo ./install_ocserv_from_package.sh [PORT] [DOMAIN] [RADIUS_IP] [RADIUS_SECRET] [DNS_CHOICE]
 
 # --- UI Color Definitions ---
-C_OFF='\033[0m'; C_RED='\033[0;31m'; C_GREEN='\033[0;32m'; C_YELLOW='\033[0;33m'; C_BLUE='\033[0;34m'; C_PURPLE='\033[0;35m'; C_CYAN='\033[0;36m'; C_BOLD='\033[1m'; C_BLINK_RED='\033[5;31m'
+C_OFF='\033[0m'; C_RED='\033[0;31m'; C_GREEN='\033[0;32m'; C_YELLOW='\033[0;33m'; C_BLUE='\033[0;34m'; C_PURPLE='\033[0;35m'; C_CYAN='\033[0;36m'; C_BOLD='\033[1m';
 
 # --- Helper Functions ---
 print_header() { echo -e "\n${C_PURPLE}${C_BOLD}====== $1 ======${C_OFF}"; }
